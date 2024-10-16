2135ceb01e696a920fadc61b4b61400b844df247
kingjames500 <jimmymaina500@gmail.com>
2024-10-17 00:41:10 +0300

    working on delete item, getting all items, getting an item

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { Command } from "commander";
 import fs from "fs";
+import chalk from "chalk";
 
 const program = new Command();
 
@@ -32,14 +33,138 @@ program
     //then I read the existing budget tracker items on the json file and save it on a variable
     const loadBudget = fs.readFileSync("./data/BudgetTracker.json", "utf-8");
 
+    let budget;
+    if (!loadBudget) {
+      budget = [];
+    }
     //convert the json  to and array
-    const budget = JSON.parse(loadBudget);
-
+    budget = JSON.parse(loadBudget);
+    //I want to make the title of the budget tracker unique so I will check if the title already exist
+    const bugdetTrackerExist = budget.find(
+      (currentBudget) => currentBudget.title === title,
+    );
+    if (bugdetTrackerExist) {
+      console.log(
+        chalk.bgRed(`The budget item with the title '${title}' already exists`),
+      );
+      return;
+    }
     //push the new budget to the array using push method
     budget.push(newBudget);
 
     //then write the new budget onn the array using the writeFileSync method
     fs.writeFileSync("./data/BudgetTracker.json", JSON.stringify(budget));
+
+    console.log(chalk.green.bgBlack("New budget item added successfully"));
+  });
+
+//this program will list all the items in the budget tracker
+
+program
+  .command("listItems")
+  .description("listing all the items in the budget tracker")
+  .action(function () {
+    //read the budgettracker json file to get all the items
+    const loadBudget = fs.readFileSync("./data/BudgetTracker.json", "utf-8");
+
+    //parse the json file to an array
+    const budget = JSON.parse(loadBudget);
+
+    //check if the budgetTracker is empty
+    if (budget.length === 0) {
+      console.log(chalk.bgYellow("Your budget tracker is empty"));
+      return;
+    }
+
+    //loop through the budget array and print the items
+    budget.forEach((currentBudget) => {
+      console.log(
+        chalk.bgGreen(
+          `Title: ${currentBudget.title} \n Quantity: ${currentBudget.quantity} \n Price: ${currentBudget.price}`,
+        ),
+      );
+    });
+  });
+
+//this program will get a specific item from the budget tracker
+program
+  .command("getItem")
+  .description("getting a specific item from the budget tracker")
+  .option("-t | --title <value>", "Title of the item")
+  .action(function (options) {
+    //declare the title variable
+    const title = options.title;
+
+    //read the budget tracker json file
+    const loadSingleBudget = fs.readFileSync(
+      "./data/BudgetTracker.json",
+      "utf-8",
+    );
+
+    //convert the json to an array
+    let singleBudget = JSON.parse(loadSingleBudget);
+
+    //validating the user input and displaying an error message if the user input is invalid
+    if (title) {
+      const budget = singleBudget.find(
+        (currentBudget) => currentBudget.title === title,
+      );
+      if (budget) {
+        console.log(chalk.green("Budget item found"));
+        console.log("=======================================");
+        console.log(budget.title);
+        console.log(budget.quantity);
+        console.log(budget.price);
+        return;
+      }
+      console.log(
+        chalk.bgRed(`The budget item with the title '${title}' does not exist`),
+      );
+      return;
+    }
+  });
+
+//this program will delete a specific item from the budget tracker
+program
+  .command("deleteItem")
+  .description("deleting a specific item from the budget tracker")
+  .option("-t | --title <value>", "Title of the item")
+  .action(function (options) {
+    const title = options.title;
+
+    const loadBudget = fs.readFileSync("./data/BudgetTracker.json", "utf-8");
+    const budget = JSON.parse(loadBudget);
+
+    if (budget.length === 0) {
+      console.log(chalk.bgYellow("Your budget tracker is empty"));
+      return;
+    }
+
+    if (title) {
+      const budgetItems = budget.filter(
+        (currentBudget) => currentBudget.title !== title,
+      );
+      if (budgetItems.length === budget.length) {
+        console.log(
+          chalk.bgRed(
+            `The budget item with the title '${title}' does not exist`,
+          ),
+        );
+        return;
+      }
+      fs.writeFileSync(
+        "./data/BudgetTracker.json",
+        JSON.stringify(budgetItems),
+      );
+      console.log(
+        chalk.bgGreen(
+          `The budget item with the title '${title}' has been deleted`,
+        ),
+      );
+      return;
+    }
+
+    // fs.writeFileSync("./data/BudgetTracker.json", JSON.stringify(budgetItems));
   });
 
 program.parse(process.argv);
